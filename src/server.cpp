9a707116b3421afaad16d9b991f547ad38ee92bd
Sushant Anand <sushantxanand@gmail.com>
2024-09-14 03:51:39 +0530

    Main development done

--- src/server.cpp
+++ src/server.cpp
@@ -5,13 +5,12 @@ int main() {
     readConfig();
     int PORT = config.port;
     int serverSocket;
-    
+
     if (createServerSocket(serverSocket) == 1) {
         std::cout << "Server listening on port " << PORT << "..." << std::endl;
     } else {
         std::cerr << "Server socket creation failed." << std::endl;
     }
-
 
     std::thread listenConnectionThread(listenConnection, serverSocket);
     std::thread serveClientThread(handleClients);
